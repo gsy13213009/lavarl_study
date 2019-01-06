806d64f5be008ecbbb8d6c106cb644c4d445fb50
郭思义 <gsy@meitu.com>
2019-01-06 21:20:43 +0800

    [optimize] 添加注册的逻辑

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller {
@@ -11,6 +12,19 @@ class RegisterController extends Controller {
     }
 
     public function register() {
+        // 验证
+        $this->validate(request(), [
+            'name' => 'required|min:3|unique:users,name',
+            'email' => 'required|unique:users,email|email',
+            'password' => 'required|min:5|max:10|confirmed'
+        ]);
+        // 逻辑
+        $name = \request('name');
+        $email = \request('email');
+        $password = bcrypt(request('password'));
+        // 渲染
+        User::create(compact('name', 'email', 'password'));
 
+        return redirect('/login');
     }
 }
